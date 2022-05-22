43c051c7310dab83ead12b08605735a387f141b7
saidugurlu <ugurlumsaid@gmx.de>
2022-05-22 16:30:19 +0200

    overlay tooltip work

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,53 +1,53 @@
-import {useContext, useEffect,useState} from "react";
-import {EmployeeContext} from '../contexts/EmployeeContext';
-import { Button, Modal } from "react-bootstrap";
+import { useContext, useEffect, useState } from "react";
+import { EmployeeContext } from "../contexts/EmployeeContext";
+import { Button, Modal, OverlayTrigger, Tooltip } from "react-bootstrap";
 import EditForm from "./EditForm";
 
 function Employee({ employee }) {
-  const{deleteEmployee} = useContext(EmployeeContext)
+  const { deleteEmployee } = useContext(EmployeeContext);
 
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-
   useEffect(() => {
     handleClose();
-}, [employee]);
-
-
+  }, [employee]);
 
   return (
     <>
+      <td>{employee.name}</td>
+      <td>{employee.email}</td>
+      <td>{employee.address}</td>
+      <td>{employee.phone}</td>
+      <td>
+        <OverlayTrigger overlay={<Tooltip id={`tooltip-top`}>Edit</Tooltip>}>
+          <button
+            onClick={handleShow}
+            className="btn text-warning btn-act"
+            data-toggle="modal"
+          >
+            <i className="material-icons">&#xE254;</i>
+          </button>
+        </OverlayTrigger>
+        <OverlayTrigger overlay={<Tooltip id={`tooltip-top`}>Delete</Tooltip>}>
+          <button
+            onClick={() => deleteEmployee(employee.id)}
+            className="btn text-danger btn-act"
+            data-toggle="modal"
+          >
+            <i className="material-icons">&#xE872;</i>
+          </button>
+        </OverlayTrigger>
+      </td>
 
-          <td>{employee.name}</td>
-          <td>{employee.email}</td>
-          <td>{employee.address}</td>
-          <td>{employee.phone}</td>
-          <td>
-            <button onClick={handleShow} className="btn text-warning" data-toggle="modal">
-              <i className="material-icons" data-toggle="tooltip" title="Edit">
-                &#xE254;
-              </i>
-            </button>
-            <button onClick={()=> deleteEmployee(employee.id)} className="btn text-danger" data-toggle="modal">
-              <i
-                className="material-icons"
-                data-toggle="tooltip"
-                title="Delete"
-              >
-                &#xE872;
-              </i>  
-            </button>
-          </td>
-
-          <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header className="modal-header" closeButton>
           <Modal.Title>Edit Employee</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <EditForm theEmployee={employee}/>
+          <EditForm theEmployee={employee} />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -55,8 +55,6 @@ function Employee({ employee }) {
           </Button>
         </Modal.Footer>
       </Modal>
-        
-    
     </>
   );
 }
